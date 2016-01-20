08a788be3d6ed84306ebdece26470f81663a9f06
Muu <muu.dev@gmail.com>
2016-01-20 12:42:30 +0100

    funtional test word exercises completed 10 ask and answer

--- app/src/androidTest/java/com/comoelagua/android/braille/WordExercisesActivityTest.java
+++ app/src/androidTest/java/com/comoelagua/android/braille/WordExercisesActivityTest.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
@@ -29,15 +30,15 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 public class WordExercisesActivityTest {
 
     @Rule
-    public ActivityTestRule<WordExercisesActivity> mActivityRule = new ActivityTestRule<>(
+    public ActivityTestRule<WordExercisesActivity> wordExercisesActivityRule = new ActivityTestRule<>(
             WordExercisesActivity.class);
 
     @Test
     public void testSetAnswerOK() {
-        String word = ((TextView) mActivityRule.getActivity().findViewById(R.id.ask)).getText().toString();
+        String word = ((TextView) wordExercisesActivityRule.getActivity().findViewById(R.id.ask)).getText().toString();
         onView(withId(R.id.answer)).perform(typeText(word), closeSoftKeyboard());
         onView(withId(R.id.next)).perform(click());
-        // OK if askNumber after click is 2
+        // OK if askNumber after click is 2, has changed to next ask
         onView(withId(R.id.askNumber))
                 .check(matches(withText(" 2")));
     }
@@ -46,8 +47,25 @@ public class WordExercisesActivityTest {
     public void testSetAnswerFAIL() {
         onView(withId(R.id.answer)).perform(typeText("anything mock"), closeSoftKeyboard());
         onView(withId(R.id.next)).perform(click());
-        // OK if askNumber after click is 1
+        // OK if askNumber after click is 1, keep in same ask
         onView(withId(R.id.askNumber))
                 .check(matches(withText(" 1")));
+    }
+
+    @Test
+    public void testCompletedExercises() {
+        TextView askTextView = (TextView) wordExercisesActivityRule.getActivity().findViewById(R.id.ask);
+        int maxSize = 10;
+        for(int i = 0; i < maxSize; i++) {
+            String word = askTextView.getText().toString();
+            onView(withId(R.id.answer)).perform(replaceText(word));
+            onView(withId(R.id.next)).perform(click());
+            if (i < maxSize - 1) {
+                // last next click start another activity, no has askNumber
+                onView(withId(R.id.askNumber))
+                        .check(matches(withText(" " + (2 + i))));
+            }
+        }
+        //onView(withId(R.id.okValue)).check(matches(withText("" + maxSize)));
     }
 }
