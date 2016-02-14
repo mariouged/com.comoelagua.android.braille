fec1309266b1a0fe746010a0f533fec543e22d64
Muu <muu.dev@gmail.com>
2016-02-14 14:20:39 +0100

    Supporting API 10 Gingerbread

--- app/src/androidTest/java/com/comoelagua/android/braille/test/PhraseExercisesActivityTest.java
+++ app/src/androidTest/java/com/comoelagua/android/braille/test/PhraseExercisesActivityTest.java
@@ -15,13 +15,9 @@
  */
 package com.comoelagua.android.braille.test;
 
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.ViewAction;
-import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.comoelagua.android.braille.PhraseExercisesActivity;
@@ -35,7 +31,6 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
--- app/src/androidTest/java/com/comoelagua/android/braille/test/WordExercisesActivityTest.java
+++ app/src/androidTest/java/com/comoelagua/android/braille/test/WordExercisesActivityTest.java
@@ -1,17 +1,11 @@
 package com.comoelagua.android.braille.test;
 
-import junit.framework.TestSuite;
-
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.internal.builders.AllDefaultPossibilitiesBuilder;
 import org.junit.runner.RunWith;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.test.runner.AndroidJUnitRunner;
-import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.widget.TextView;
 
@@ -24,7 +18,6 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
